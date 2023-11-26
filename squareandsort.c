4bb43bf7e4218a27824c3830e9f8b3c5f9214bc1
Winfred <winfrednginakilonzo@gmail.com>
2023-11-27 00:16:22 +0300

    Implemented sorting of array of ints squares

--- squareandsort.c
+++ squareandsort.c
@@ -2,21 +2,40 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-    for (int i = 0; i < numsSize; i++)
+    int swap;
+    int i;
+    int j;
+    int k;
+    for (i = 0; i < numsSize; i++)
     {
         nums[i] *= nums[i];
     }
-    for (int j = 0; i < numsSize - 1; j++)
+    for (j = 0; j < numsSize - 1; j++)
     {
-        int key = nums[j];
-        for (int k = 1; k < numsSize; k++)
+        swap = 0;
+        for (k = 1; k < (numsSize - j); k++)
         {
-            if (key < nums[k])
+            if (nums[k - 1] > nums[k])
             {
-                
+                int temp = nums[k - 1];
+                nums[k - 1] = nums[k];
+                nums[k] = temp;
+                swap = 1;
             }
         }
-        
+        if (swap == 0)
+        {
+            break;
+        }
     }
-    
+    // Allocate memory for the result array
+    int* result = malloc(numsSize * sizeof(int));
+
+    // Copy the sorted squared values to the result array
+    memcpy(result, nums, numsSize * sizeof(int));
+
+    // Set the returnSize pointer to the size of the result array
+    *returnSize = numsSize;
+
+    return result;
 }
