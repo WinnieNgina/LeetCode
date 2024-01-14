116a6a5466fb84d0945427370f7e8846dbc95e5c
Winfred <winfrednginakilonzo@gmail.com>
2024-01-14 10:36:15 +0300

    Find maximum length using sliding window

---
+++ sliding_window.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Use the concept of sliding window to find the maximum subarray"""
+from typing import List
+
+
+def find_length(nums: List[int], k: int) -> int:
+    left = sums = length = 0
+    for right in range (len(nums)):
+        sums += nums[right]
+        while sums > k:
+            sums -= nums[left]
+            left += 1
+        length = max(length, right - left + 1)
+    return length
+
+
+nums2 = [2, -1, 2, 1, -3, 2, 1]
+k2 = 4
+result2 = find_length(nums2, k2)
+print(f"Test 2: {result2}")  # Expected output: 5
